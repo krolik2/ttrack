8e01aad1ca8b578c30312174f37ae47691ce97ae
Mateusz Krolikowski <krolikmat@gmail.com>
2023-03-03 14:05:40 +0100

    updated list with new tasks

--- src/data/data.ts
+++ src/data/data.ts
@@ -3,8 +3,13 @@ export const taskData = [
   { name: "task2", id: 2, targetPerHour: 13.1 },
   { name: "task3", id: 3, targetPerHour: 104.65 },
   { name: "task4", id: 4, targetPerHour: 35.77 },
-  { name: "task5", id: 5, targetPerHour: 47.89 },
+  { name: "task5", id: 5, targetPerHour: 30 },
   { name: "task6", id: 6, targetPerHour: 20 },
   { name: "task7", id: 7, targetPerHour: 78.45 },
   { name: "task8", id: 8, targetPerHour: 45.77 },
+  { name: "task9", id: 9, targetPerHour: 35.21 },
+  { name: "task10", id: 10, targetPerHour: 35.92 },
+  { name: "task11", id: 11, targetPerHour: 21.97 },
+  { name: "task12", id: 12, targetPerHour: 42.25 },
+  
 ];
